2039843a0503fa4158b226f0a389d44fe1591026
Charlie <charlie.islas@gmail.com>
2019-01-11 15:02:09 -0600

    uploading with the necessary changes for mongolab

--- server.js
+++ server.js
@@ -29,9 +29,19 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.Promise = Promise; // Set mongoose to leverage Built in JavaScript ES6 Promises
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect("mongodb://heroku_73f4n636@ds133570.mlab.com:33570/heroku_73f4n636",{
+  useMongoClient:true
+});
+
+let mongooseConnection = mongoose.connection;
+
+mongooseConnection.on('error', console.error.bind(console, 'connection error:'));
+mongooseConnection.once('open', function() {
+  console.log(`Sucessfully Connected to Mongo DB !`); // If Connection is successful, Console.log(Message)
+});
 
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
